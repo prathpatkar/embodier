cb2db7e15f395ae983070fed393e336ad59607bd
prathamesh patkar <ppatkar@perinc.com>
2021-08-24 02:22:36 -0500

    added source code url

--- setup.py
+++ setup.py
@@ -10,11 +10,14 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="embodier",
-    version="1.0.3",
+    version="1.0.4",
     description="It generates the unique avatar, which save image into PNG, JPG, or Base64 string. you can use this package to give avatar to newly registered users on your application.",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/2000prath/embodier",
+    project_urls={
+          "Source Code": "https://github.com/2000prath/embodier",
+    },
     author="Prathamesh Patkar",
     author_email="2000prath@gmail.com",
     license="MIT",
