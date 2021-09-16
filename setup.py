ee13c62e6acf0b385ee4b7ee97e376a088ec8b6b
prathamesh patkar <ppatkar@perinc.com>
2021-09-16 06:00:26 -0500

    updated version number

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="embodier",
-    version="1.0.4",
+    version="1.0.5",
     description="It generates the unique avatar, which save image into PNG, JPG, or Base64 string. you can use this package to give avatar to newly registered users on your application.",
     long_description=README,
     long_description_content_type="text/markdown",
